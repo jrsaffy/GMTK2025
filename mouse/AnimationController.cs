f7fef18d5ac2e2b480b0bc6f744e2e66fe7a15dc
Josh S <jrsaffy@gmail.com>
2024-08-16 21:44:27 -0400

    Mouse movement, animation, eatting input

---
+++ mouse/AnimationController.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+public partial class AnimationController : AnimatedSprite2D
+{
+
+	public Vector2 direction;
+	// Called when the node enters the scene tree for the first time.
+	int facing = 1; //Right = 1, left = -1
+	string animation = "idle";
+
+	public bool eating = false;
+
+	void setXDirection()
+	{
+		float x = direction.X;
+		GD.Print(x);
+		if(x > 0)
+		{
+			facing = 1;
+			FlipH = false;
+		}
+		if(x < 0)
+		{
+			facing = -1;
+			FlipH = true;
+		}
+	}
+
+	void setWalking()
+	{
+		if (direction.Length() == 0)
+		{
+			animation = "idle";
+		}
+		else
+		{
+			animation = "walk";
+		}
+	}
+
+
+	public override void _Ready()
+	{
+		Play("idle");
+	}
+
+	void playAnimation()
+	{
+		// GD.Print(animation);
+		Play(animation);
+	}
+
+	void eat()
+	{
+		if(eating)
+		{
+			animation = "eat";
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		setXDirection();
+		setWalking();
+		eat();
+		playAnimation();
+	}
+}
---
+++ mouse/mouse.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public partial class mouse : CharacterBody2D
+{
+	
+	int speed = 75;
+
+	AnimationController animation_controller;
+	
+
+	public void move()
+	{
+		Vector2 direction = getDirection();
+		Velocity = speed * direction;
+		MoveAndSlide();
+		
+	}
+
+	void eat()
+	{
+		if (Input.IsActionPressed("eat"))
+		{
+			animation_controller.eating = true;
+		}
+		else
+		{
+			animation_controller.eating = false;
+		}
+	}
+
+	public Vector2 getDirection()
+	{
+		int up = Convert.ToInt32(Input.IsActionPressed("up"));
+		int down = Convert.ToInt32(Input.IsActionPressed("down"));
+		int left = Convert.ToInt32(Input.IsActionPressed("left"));
+		int right = Convert.ToInt32(Input.IsActionPressed("right"));
+
+		Vector2 direction = new Vector2(right - left, down - up).Normalized();
+		animation_controller.direction = direction;
+
+		return direction;
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{	
+		animation_controller = GetNode<AnimationController>("AnimationController");
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		move();
+		eat();
+	}
+}
